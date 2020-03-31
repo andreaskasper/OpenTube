f6e5755725cac9fadec7f4f2edebf7a010a655f9
Andreas Kasper <andreas.kasper@goo1.de>
2020-03-31 03:51:08 +0200

    Weitere verbesserungen an Klassen DB und MyUser

--- public_html/app/code/classes/DB.php
+++ public_html/app/code/classes/DB.php
@@ -9,8 +9,6 @@ class DB {
 
     public static function init(int $id, string $connectionstring) {
         self::$_cache[$id]["connectionstring"] = $connectionstring;
-
-        print_r(PDO::getAvailableDrivers());
     }
 
     public function __construct(int $id) {
@@ -18,10 +16,19 @@ class DB {
         $this->conn = new PDO(self::$_cache[$id]["connectionstring"]);
     }
 
-    public function cmdrow(string $sql, Array $values = array()) : Array {
-        $rows = $this->conn->query($sql);
-        print_r($rows);
-        return array();
+    public function __get($name) {
+        switch(strtolower($name)) {
+            case "scheme": $g = explode(":", self::$_cache[$id]["connectionstring"]); return $g[0];
+        }
+        trigger_error("Variable not found ".$name, E_USER_WARNING);
+    }
+
+    public function cmdrow(string $sql, Array $values = array()) {
+        $sth = $this->conn->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute($values);
+        $row = $sth->fetch(PDO::FETCH_BOTH);
+        print_r($row);
+        return $row;
     }
 
 
--- public_html/app/code/classes/MyUser.php
+++ public_html/app/code/classes/MyUser.php
@@ -16,8 +16,12 @@ class MyUser {
 
     public static function login_emailpassword(string $email, string $password) : int {
         $db = new DB(0);
-        $db->cmdrow('SELECT * FROM users WHERE email = "{0}" LIMIT 0,1', array($email));
-        return 2;
+        $row = $db->cmdrow('SELECT T1.id,T2.value FROM users as T1 LEFT JOIN users_logins as T2 ON T1.id=T2.user_id AND T2.provider = "ep" WHERE email = ? LIMIT 0,1', array($email));
+        print_r($row);
+        if (empty($row["id"])) return -1;
+        if ($row["value"] != $password) return -2;
+        $_SESSION["myuser"]["id"] = $row["id"];
+        return 1;
     }
 
     public static function logout() : bool {
