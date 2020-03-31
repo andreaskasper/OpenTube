4913c009328d9f6fc143ec187f0ee0ce407fe618
Andreas Kasper <andreas.kasper@goo1.de>
2020-03-31 02:25:14 +0200

    Datenbank Klasse, main.css,main.js

---
+++ public_html/app/code/classes/DB.php
@@ -0,0 +1,22 @@
+<?php
+
+class DB {
+
+    
+
+    public static function init(int $id, string $connectionstring) {
+
+
+    }
+
+    public function __construct(int $id) {
+
+
+    }
+
+
+
+
+
+}
+
